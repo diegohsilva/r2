2bcc5ef1af40e8c895a7c3d4f746ec774d38e262
Daniel Lirio Reis <daniellirioreis@gmail.com>
2017-08-06 23:25:24 -0300

    Troca login forma de login

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
 //= require turbolinks
+//= require jquery
+//= require bootstrap-sprockets
 //= require jquery.min
-//= require bootstrap
-//= require bootstrap.min
 //= require_tree .
-
-
